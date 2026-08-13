2a0d4c61405977efa614f9ed57dbc28edfc3f446
yahooghoub-tech <yahooghoub@gmail.com>
2026-08-13 20:47:15 +0330

    update website

--- attendance-6-1.js
+++ attendance-6-1.js
@@ -61,10 +61,14 @@ document.getElementById("message");
 
 function getToday(){
 
-const date=
-new Date();
-
-return date.toISOString().split("T")[0];
+    return new Intl.DateTimeFormat(
+        "fa-IR-u-nu-latn",
+        {
+            year:"numeric",
+            month:"2-digit",
+            day:"2-digit"
+        }
+    ).format(new Date());
 
 }
 
